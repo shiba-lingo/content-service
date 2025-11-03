a3144cdb69fd9f17cd7dcfbba8ca36bda1d96e52
chaoyuuu <chaoyu2330@gmail.com>
2025-11-03 13:18:43 -0800

    add source article schema

---
+++ data/sourceArticle.js
@@ -0,0 +1,36 @@
+import mongoose, {Schema, Types} from 'mongoose';
+
+const SOURCES = ['BBC']
+
+const sourceArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title is required.'],
+        maxlength: [100, 'Title cannot be more than 200 characters.'],
+    },
+
+    content: {
+        type: String,
+        required: [true, 'Article content is required.'],
+    },
+
+    source_url: {
+        type: String,
+        required: [true, 'source_url is required.'],
+    },
+
+    source: {
+        type: String,
+        required: [true, 'source is required.'],
+        enum: {
+            values: SOURCES,
+            message: '{VALUE} is not a supported source.',
+        },
+    },
+
+    published_at: { type: Date },
+}, { timestamps: true, versionKey: false });
+
+
+const SourceArticle = mongoose.model('sourceArticle', sourceArticleSchema);
+export default SourceArticle;
