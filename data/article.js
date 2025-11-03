1a0d8f9ee561f8d1a77aea80f1f2c9c3600e742b
chaoyuuu <chaoyu2330@gmail.com>
2025-11-03 13:18:33 -0800

    update article schema

--- data/article.js
+++ data/article.js
@@ -1,45 +1,48 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, {Schema, Types} from 'mongoose';
+
+const CATEGORIES = ['technology', 'history', 'news']
+const LANGUAGE_LEVELS = ['easy', 'medium', 'hard']
 
 const articleSchema = new Schema({
-    // Unique Article Title
     title: {
         type: String,
         required: [true, 'Title is required.'],
-        trim: true,
-        maxlength: [200, 'Title cannot be more than 200 characters.'],
+        maxlength: [100, 'Title cannot be more than 200 characters.'],
     },
 
-    // The main content of the article
     content: {
         type: String,
         required: [true, 'Article content is required.'],
-        minlength: [50, 'Content must be at least 50 characters long.'],
     },
 
-    // Category (used by the Learning Service for filtering/recommendation)
+    summary: {
+        type: String,
+        required: [true, 'Article content is required.'],
+    },
+
     category: {
         type: String,
         required: [true, 'Category is required.'],
         enum: {
-            values: ['Technology', 'History', 'News'], // Enforce specific categories
+            values: CATEGORIES, // Enforce specific categories
             message: '{VALUE} is not a supported category.',
         },
     },
 
-    // English Reading Level (crucial for the Learning Service)
-    english_level: {
+    level: {
         type: String,
         required: [true, 'English level is required.'],
         enum: {
-            values: ['Easy', 'Medium', 'Hard'], // Common CEFR levels
+            values: LANGUAGE_LEVELS,
             message: 'Level must be one of Easy, Medium, Hard.',
         },
     },
 
-    // Article Author
-    author: { type: String },
-
-}, { collation: 'article'});
+    source_id: {
+        type: Types.ObjectId,
+        required: [true, 'SourceId is required.'],
+    }
+}, { timestamps: true, versionKey: false });
 
 
 const Article = mongoose.model('article', articleSchema);
