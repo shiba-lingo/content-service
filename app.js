91fad35bb017edf4c9713cc803c43c0bf378ffd8
chaoyuuu <chaoyu2330@gmail.com>
2025-11-09 14:32:45 -0800

    extract swagger config

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import contentRouter from "./routes.js";
-// import swaggerRouter from "./swagger.js";
 import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
-import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./swagger.js";
 
 dotenv.config();
 
@@ -21,33 +20,8 @@ mongoose.connect(MONGO_URL)
     .then(() => console.log('Successfully connected to MongoDB!'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Content API',
-            version: '1.0.0',
-            description: 'API documentation for content service',
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000/',
-            },
-        ],
-    },
-    apis: ['./routes.js'], // adjust path
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-app.use('/api/content', contentRouter);
-
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
-// app.use('/docs', swaggerRouter);
-
+app.use('/contents', contentRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
