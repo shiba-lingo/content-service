23869ee2336c419112cc5709ee8704229603f1ec
chaoyuuu <chaoyu2330@gmail.com>
2025-11-09 14:46:08 -0800

    extract swagger config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import swaggerUi from "swagger-ui-express";
-import swaggerSpec from "./swagger.js";
+import swaggerSpec from "./config/swagger.js";
 
 dotenv.config();
 
